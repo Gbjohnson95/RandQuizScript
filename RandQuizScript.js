bb17ee0bcdfea740281f694b22a3faab0caef1e2
Grant Johnson <Gbjohnson95@users.noreply.github.com>
2016-02-24 15:13:37 -0700

    Yolo

--- RandQuizScript.js
+++ RandQuizScript.js
@@ -1,6 +1,6 @@
 var questions = $('.question');
 $("div[class='question']").hide();
-$(questions).last().after('<div class="name" style="display: none;">{LastName}</div>');
-var Entropy = (($("div.name")[0].innerHTML.charCodeAt("0") * (3 + 3)) - 65) % questions.length; // 3 is where the attempt number will go
+$(questions).last().after('<div class="UserId" style="display: none;">{UserId}</div>');
+var Entropy = (($("div.UserId")[0].innerHTML * (3 + 3)) - 65) % questions.length; // 3 is where the attempt number will go
 $(questions[Entropy]).show();
 $(questions).filter("[style='display: none;']").remove();
