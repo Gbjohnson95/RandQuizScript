e7b5040f1fe1a123705fa8684cdeb3bd64e6b6c8
Grant Johnson <Gbjohnson95@users.noreply.github.com>
2016-02-25 09:33:16 -0700

    Now operates on a time/userId function

--- RandQuizScript.js
+++ RandQuizScript.js
@@ -1,6 +1,8 @@
 var questions = $('.question');
 $("div[class='question']").hide();
 $(questions).last().after('<div class="UserId" style="display: none;">{UserId}</div>');
-var Entropy = (($("div.UserId")[0].innerHTML * (3 + 3)) - 65) % questions.length; // 3 is where the attempt number will go
+var date = new Date();
+var dateTime = date.getDate() + "" + parseInt((date.getHours()+3)/4);
+var Entropy = (($("div.UserId")[0].innerHTML * (dateTime + 1))) % questions.length;
 $(questions[Entropy]).show();
 $(questions).filter("[style='display: none;']").remove();
