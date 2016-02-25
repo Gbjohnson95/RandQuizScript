2832fd66f32a550d0183cdb2da4442473048e2cc
Grant Johnson <Gbjohnson95@users.noreply.github.com>
2016-02-25 09:37:39 -0700

    hmmm

--- RandQuizScript.js
+++ RandQuizScript.js
@@ -3,6 +3,6 @@ $("div[class='question']").hide();
 $(questions).last().after('<div class="UserId" style="display: none;">{UserId}</div>');
 var date = new Date();
 var dateTime = date.getDate() + "" + parseInt((date.getHours()+3)/4);
-var Entropy = (($("div.UserId")[0].innerHTML * (dateTime + 1))) % questions.length;
+var Entropy = ($("div.UserId")[0].innerHTML * (dateTime + 1)) % questions.length;
 $(questions[Entropy]).show();
 $(questions).filter("[style='display: none;']").remove();
