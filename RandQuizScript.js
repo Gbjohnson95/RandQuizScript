5d36d960fa96be136595d9adad85d7753f36304d
Grant Johnson <Gbjohnson95@users.noreply.github.com>
2016-02-26 11:32:44 -0700

    Now fully working and commented

--- RandQuizScript.js
+++ RandQuizScript.js
@@ -1,54 +1,43 @@
-// GOALS
-// - Shouldn't count on JS hacks and tricks.
-// - As completely easy to impliment as possible. Shouldn't need a unique ID for every set of questions. A container DIV may be needed.
-// - Script should be as small as possible.
-
-// TO-DO
-// - Script only works with one question displayed. Needs a method to only filter one set of possible questions at a time. Unavoidable
-//   on results view.
-// - Try and make the UserID API call once, then store it as a variable on the page.
-
-// Check for a label that is located on the review page that says what time the quiz was taken
-// I do NOT like this function, relies on BS not changing anything.
-if ($("label").length > 0) {
-    if ($("label")[0].innerHTML.indexOf("Written:") === 0) { // Label in question has the started date.
-        var datestring = $("label")[0].innerHTML.substring(13);
-        var day = datestring.substring(0, 2);
-        var hour = parseInt(datestring.substring(8, 11));
-        var dateT = day + "" + parseInt((hour + 3) / 4);
-    } else { // If a label is found, but not the one with the time, just use the current time
-        var curDate = new Date();
-        var dateT = curDate.getDate() + "" + parseInt((curDate.getHours() + 3) / 4);
-    }
-} else { // if no labels are found, just use the current time
-    var curDate = new Date();
-    var dateT = curDate.getDate() + "" + parseInt((curDate.getHours() + 3) / 4);
+$(".question").hide(); // Hide everything that is question text
+if ($("#UserID").length === 0) { // If an elem with a UserID exists, dont call the API again
+	$.getJSON({
+		url : "/d2l/api/lp/1.7/users/whoami",
+		async : false // Only way to ensure that with multiple scripts on the page only call the API once per page
+	}, function (data) {
+		$('body').append('<div id="UserID" style="display: none;">' + data.Identifier + '</div>'); // Set an element to contain the UserID
+	});
 }
+if ($("label").length > 0) {
+	if ($("label")[0].innerHTML.indexOf("Written:") === 0) {
+		var datestring = $("label")[0].innerHTML.substring(13); // Label that displays the time of the quiz being taken
+		var day = datestring.substring(0, 2); // Pulls the day of month out
+		var hour = parseInt(datestring.substring(8, 11)); // Uses parseInt and pulls 3 chars so that if the date is displayed as 9 instead of 09 it still functions
+		var dateT = day + "" + parseInt((hour + 3) / 4); // DDQ, where DD is the day, and Q is the quater of the day with an offset of 3 hours
+	} else { // If a label is found but not the one that were looking for
+		// The + "" + is needed to concat the nums rather than add them
+		var curDate = new Date(); //  Get current date
+		var dateT = curDate.getDate() + "" + parseInt((curDate.getHours() + 3) / 4); // DDQ, where DD is the day, and Q is the quater of the day with an offset of 3 hours
+	}
+} else {
+	// The + "" + is needed to concat the nums rather than add them
+	var curDate = new Date(); // Get current date
+	var dateT = curDate.getDate() + "" + parseInt((curDate.getHours() + 3) / 4); // DDQ, where DD is the day, and Q is the quater of the day with an offset of 3 hours
+}
+$(".questions").each(function () { // Gets the question blocks
+	questions = $(this).children(".question"); // Gets the question text in each of the questions
+	if ($("#UserID").length > 0) { // if there is an elem with the user ID then use it for the
+		UserID = $("div[id='UserID']").first().text(); // Get the UserID
+		$(questions[(UserID * (dateT + 1)) % questions.length]).show(); //  Show the selected question
+		$(questions).filter("[style='display: none;']").remove(); // Delete the other questions
+		$(questions).removeAttr("class"); // Drop the class for some reason
+	} else { // If theres no UserID then the function to get it failed
+		console.error("If you're reading this something done broke and random question texts where displayed");
+		$(".questions").each(function () {
+			questions = $(this).children(".question");
+			$(questions[parseInt(Math.floor((Math.random() * questions.length) + 1))]).show();
+			$(questions).filter("[style='display: none;']").remove();
+			$(questions).removeAttr("class");
+		});
 
-questions = $('.question'); // Populate the questions
-questions.hide(); // Hide all the questions
-var success = false; // Preset the success
-
-// Get the UserID. First iteration of this script used a replace string, but when hosting the script outside of BS script has trouble 
-// actualy displaying the replacestring, so API it is. 
-$.getJSON("/d2l/api/lp/1.7/users/whoami", function (data) {
-    var UserID = data.Identifier; // Get the UserID
-    var Entropy = (UserID * (dateT + 1)) % questions.length; // Generate a sorta random #.
-    if (!success) { // If the timout function hasnt run yet
-        success = true; // Make sure the timeout function doesn't run.
-        $(questions[Entropy]).show(); // Show the selected question
-        $(questions).filter("[style='display: none;']").remove(); // Drop the other elements
-        $(questions).removeAttr("class"); // Clean up element. Students should be completely blind on this.
-    }
+	}
 });
-
-// If the function above fails to get the json in under 5 seconds, just pick one at random.
-setTimeout(function () {
-    if (!success) {
-        success = true;
-        var Entropy = parseInt(Math.floor((Math.random() * questions.length) + 1));
-        $(questions[Entropy]).show(); // Show that element
-        $(questions).filter("[style='display: none;']").remove();
-        $(questions).removeAttr("class");
-    }
-}, 5000);
