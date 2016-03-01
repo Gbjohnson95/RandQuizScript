9ffffc53cdc92db4d898ba145a9e0a9f2bea23cc
Grant Johnson <Gbjohnson95@users.noreply.github.com>
2016-03-01 16:24:47 -0700

    Now Async

--- RandQuizScript.js
+++ RandQuizScript.js
@@ -1,51 +1,90 @@
-setFallbackMode(); // If all fails, only the first question will be shown.
-worked = false; // Checker for if the script failed
-if (window.jQuery) { // If JQuery is running
-    $(".question").hide(); // Hide all questions
-    if ($("#UserID").length === 0) { // Look for an element that contains the UserID
-        $.getJSON({ 
-            url: "/d2l/api/lp/1.7/users/whoami",
-            async: false // As much as I don't like this its the only way to ensure the API is only called once per page.
-        }, function (data) {
-            $('body').append('<div id="UserID" style="display: none;">' + data.Identifier + '</div>'); // Set the UserID to an element
-        });
-    }
-    if ($("label").length > 0) { // Looks for labels. Labels are used to display time stamp
-        if ($("label")[0].innerHTML.indexOf("Written:") === 0) { // Look for a timestamp
-            var datestring = $("label")[0].innerHTML.substring(13); // Get the datestring
-            var dateT = datestring.substring(0, 2) + "" + parseInt((parseInt(datestring.substring(8, 11)) + 3) / 4); // Generate a DDQ format time stamp. DD is day, Q is quarter of the day.
-        } else {
-            var curDate = new Date(); // Get current date
-            var dateT = curDate.getDate() + "" + parseInt((curDate.getHours() + 3) / 4);
-        }
-    } else {
-        var curDate = new Date();
-        var dateT = curDate.getDate() + "" + parseInt((curDate.getHours() + 3) / 4);
-    }
-    $(".questions").each(function () { // For each question container
-        questions = $(this).children(".question"); // Get it's child questions
-        UserID = $("div[id='UserID']").first().text(); // Get the UserID
-        $(questions[(UserID * (dateT + 1)) % questions.length]).show(); //Display the selected question
-        $(questions).filter("[style='display: none;']").remove(); // Remove the hidden questions
-        $(questions).removeAttr("class"); // Clean the question
-        $(this).removeAttr("class"); // Clean the container
-    });
-    worked = true; //Function completed successfully
+setFallbackMode();
+if (window.jQuery) {
+	// Leave a marker for other script to know not to run
+	$(".question").hide();
+	var curattempt;
+	if ($("#CurAttempt").length === 0 && $("#UserId").length === 0) {
+		$('body').append('<div id="CurAttempt" style="display: none;"></div>');
+		$('body').append('<div id="UserId" style="display: none;"></div>');
+	}
+	if ($("#UserId").attr("progress") == null) {
+		$("#UserId").attr("progress", "started");
+		console.log("Getting whoami");
+		$.getJSON("/d2l/api/lp/1.7/users/whoami", function(data) {
+			console.log("Got whoami");
+			UserId = data.Identifier;
+			$("#UserId").html(UserId);
+			$("#UserId").attr("progress", "complete");
+			selectQuestions();
+		});
+	}
+	if ($("#CurAttempt").first().attr("progress") == null) {
+		$("#CurAttempt").attr("progress", "started");
+		if (getParameterByName("qi") == null) {
+			var quizurl =
+				"https://byui.brightspace.com/d2l/lms/quizzing/user/quiz_summary.d2l?qi=" +
+				getParameterByName("ci") + "&ou=" + getParameterByName("ou");
+		} else {
+			var quizurl =
+				"https://byui.brightspace.com/d2l/lms/quizzing/user/quiz_summary.d2l?qi=" +
+				getParameterByName("qi") + "&ou=" + getParameterByName("ou");
+		}
+		console.log("Getting Attempt: " + quizurl);
+		$.get(quizurl, function(data) {
+			console.log("Got Attempt");
+			var attemptlabel = $("<div />").html(data).find("#z_m")[0].innerHTML;
+			var curattempt = parseInt(attemptlabel.substr(attemptlabel.indexOf(
+				"Completed - ") + 12, 2)) + 1;
+			if (isNaN(curattempt)) {
+				curattempt = 0;
+			}
+			$("#CurAttempt").html(curattempt);
+			$("#CurAttempt").attr("progress", "complete");
+			selectQuestions();
+		});
+	}
+
+	function selectQuestions() {
+			console.log("UserId Progress: " + $("#UserId").attr("progress") +
+				"CurAttempt Progress: " + $("#CurAttempt").attr("progress"));
+			if ($("#UserId").attr("progress") == "complete" && $("#CurAttempt").attr(
+				"progress") == "complete") {
+				$(".questions").each(function() {
+					curattempt = parseInt($("#CurAttempt").html()) + 1;
+					questions = $(this).children(".question");
+					console.log("UserId" + UserId);
+					console.log("CurAttempt" + curattempt);
+					console.log("Questions" + questions.length);
+					console.log("result" + (UserId * (curattempt)) % questions.length);
+					$(questions[(UserId * (curattempt)) % questions.length]).show();
+					$(questions).filter("[style='display: none;']").remove();
+					$(questions).removeAttr("class");
+					$(this).removeAttr("class");
+				});
+			}
+		}
+		// Gets a URL param
+
+	function getParameterByName(name) {
+		var url = (window.location != window.parent.location) ? document.referrer :
+			document.location;
+		name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
 }
-
-setTimeout(function () { // If the function times out
-    if (!worked) { // If still failed
-        setFallbackMode(); // Run the fallback
-    }
-}, 3000);
-
+// Vanilla JS function
 function setFallbackMode() {
-    var allQuestions = document.querySelectorAll(".question"); // Get all the questions
-    for (var i = 0; i < allQuestions.length; i++) {
-        allQuestions[i].setAttribute("style", "display: none;") // Hide all the questions
-    }
-    var defaultQuestions = document.querySelectorAll(".questions .question:first-child"); // Get the first questions
-    for (var i = 0; i < defaultQuestions.length; i++) {
-        defaultQuestions[i].setAttribute("style", "display: block;"); // Display the first question
-    }
+	var allQuestions = document.querySelectorAll(".question");
+	for (var i = 0; i < allQuestions.length; i++) {
+		allQuestions[i].setAttribute("style", "display: none;")
+	}
+	var defaultQuestions = document.querySelectorAll(
+		".questions .question:first-child");
+	for (var i = 0; i < defaultQuestions.length; i++) {
+		defaultQuestions[i].setAttribute("style", "display: block;");
+	}
 }
